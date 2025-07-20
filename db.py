9518e3f074cc7d1228bec21a25ff4b5da360ccad
omri zaher <omriza5@gmail.com>
2025-07-20 11:00:05 +0300

    fix: correct database URL in db.py

--- db.py
+++ db.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./preductions.db"
+DATABASE_URL = "sqlite:///./predictions.db"
 
 engine = create_engine(
     DATABASE_URL,
